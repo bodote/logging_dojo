ef1283fd9548d58ae0e78fcfd91dc321a6f4953c
Bodo Teichmann <bodo.teichmann@brandad-systems.de>
2023-07-28 18:23:38 +0200

    merged jacoco report from unit and integration tests

--- src/test/java/com/laurentiuspilca/ssia/domain/MyFirstClassTest.java
+++ src/test/java/com/laurentiuspilca/ssia/domain/MyFirstClassTest.java
@@ -1,12 +1,22 @@
 package com.laurentiuspilca.ssia.domain;
 
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@Slf4j
 class MyFirstClassTest {
+    @BeforeAll
+    public static void setup() {
+        log.info("before all MyFirstClassTest");
+        //        System.getProperties().forEach((key, value) -> log.info(key + ": " + value));
+    }
+
     @Test
     void myTest() {
+
         MyFirstClass firstClass = new MyFirstClass(2);
         assertThat(firstClass.timesTwo(4)).isEqualTo(16);
     }
--- src/test/java/com/laurentiuspilca/ssia/domain/MySecondClassIT.java
+++ src/test/java/com/laurentiuspilca/ssia/domain/MySecondClassIT.java
@@ -1,15 +1,16 @@
 package com.laurentiuspilca.ssia.domain;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@Slf4j
 class MySecondClassIT {
     @Test
-    void mySecondTest(){
+    void mySecondTest() {
+        log.info(" mySecondTest");
         MySecondClass mySecondClass = new MySecondClass(3);
         assertThat(mySecondClass.qubed(3)).isEqualTo(27);
-
     }
-
 }
